6b5bc08610f34aa8e1e813f7394a7164027f1e69
X-LongND2 <longnd2@bsscommerce.com>
2024-05-08 14:09:03 +0700

    Base code

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{liquid,js}",
+    "./tailwind/pre-process-template/**/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
