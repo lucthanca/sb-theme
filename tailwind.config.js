6064788b169371e7538f02f569dff066118f07ce
X-LongND2 <longnd2@bsscommerce.com>
2024-05-16 17:16:14 +0700

    Media with text new base code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     "./src/**/*.{liquid,js}",
@@ -24,5 +25,9 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant("promotion", ".promotion&"); 
+    })
+  ],
 }
